7eb6982ef0b90babbc720699823636198c451021
tianyu19920816 <1848058113@qq.com>
2025-08-27 22:19:19 +0800

    #

--- app/components/FluxKontextPro.tsx
+++ app/components/FluxKontextPro.tsx
@@ -1383,7 +1383,7 @@ export default function FluxKontextPro({ initialModel = 'pro' }: FluxKontextProP
   }, [language, processImageFile])
 
   return (
-    <div className="min-h-screen p-4 sm:p-6 lg:p-8">
+    <div className="min-h-screen p-4 sm:p-6 lg:p-8" data-page="gen">
       <div className="max-w-7xl mx-auto">
         {/* 头部 */}
         <div className="text-center mb-8">
